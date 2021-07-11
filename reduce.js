3d575a0fed40b872775573e742638f56ef077b60
martinbannister <martin.bannister91@gmail.com>
2021-07-11 21:46:24 +0100

    completed the reduce exercises

--- reduce.js
+++ reduce.js
@@ -1,4 +1,4 @@
-  
+
 /**
  * To run this file in Gitpod, use the 
  * command node reduce.js in the terminal
@@ -6,6 +6,21 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+// acc means accumulator, represents the value that will ultmately be returned from the reduce method
+// curr means current value
+
+// the reduce below can be made more concise as the line below (without the extra logging)
+/* let sum = nums.reduce((acc, curr) => acc + curr, 0); */
+let sum = nums.reduce((acc, curr) => {
+  console.log(
+    'Accumulator:', acc,
+    'Current Value:', curr,
+    'Total:', acc + curr
+  );
+  return acc + curr
+}, 0);
+console.log(sum);
 
 
 const teamMembers = [
@@ -32,6 +47,22 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 
 // Grouping by a property, and totaling it too
+// Totals the years experience for each person in the same profession
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession; //Sets the variable key to the current profession in the object being evaluated
+  if (!acc[key]) { //uses square bracket notation to check if the profession exists as a key in the accumulator object
+    // if the profession isn't a ket then create it by assigning it the years experience of the current object
+    acc[key] = curr.yrsExperience;
+  } else {
+    // if the profession key does exist then add the current object years experience to it
+    acc[key] += curr.yrsExperience;
+  }
+  return acc; // return the accumulator object
+}, {});
+
+console.log(experienceByProfession);
